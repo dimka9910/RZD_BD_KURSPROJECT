c42ceec220c20b3053868d905cfc8906a39b0495
dimka <dimka99-10@mail.ru>
2020-12-09 12:29:27 +0200

    Ну всё

---
+++ RZD/Registration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace RZD
+{
+  public partial class Registration : Form
+  {
+    private OleDbConnection cn;
+    
+    public Registration(OleDbConnection cn)
+    {
+      InitializeComponent();
+      this.cn = cn;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (textBox1.Text == "" || textBox2.Text == "")
+      {
+        MessageBox.Show("Заполните все поля", "ERROR", MessageBoxButtons.OK);
+        return;
+      }
+      
+      OleDbCommand cmd = new OleDbCommand("LoginUser", cn);
+      cmd.CommandType = CommandType.StoredProcedure;
+      cmd.Parameters.AddWithValue("@Login", textBox1.Text);
+      cmd.Parameters.AddWithValue("@Password", textBox2.Text);
+      OleDbDataReader reader = cmd.ExecuteReader();
+      
+      if (reader.Read())
+      {
+        string role = reader[2].ToString();
+        if (role == "WRONG PASSWORD")
+        {
+          MessageBox.Show("Такой login занят", "ERROR", MessageBoxButtons.OK);
+          return;
+        }
+      }
+      
+      OleDbCommand cmd1 = new OleDbCommand("Insert into Accounts (Login, Password) VALUES (?,?)", cn);
+      cmd1.Parameters.Add("@p1", OleDbType.VarChar);
+      cmd1.Parameters.Add("@p2", OleDbType.VarChar);
+      cmd1.Parameters[0].Value = textBox1.Text;
+      cmd1.Parameters[1].Value = textBox2.Text;
+      cmd1.ExecuteNonQuery();
+      MessageBox.Show("Аккаунт успешно создан", "SUCCESS", MessageBoxButtons.OK);
+      this.Hide();
+      MainForm u = new MainForm();
+      u.Show();
+    }
+
+    private void Registration_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      this.Hide();
+      MainForm u = new MainForm();
+      u.Show();
+    }
+  }
+}
